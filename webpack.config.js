0a0d9360155f3bb19fa0811bc2599aaa6f406e77
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-24 15:31:46 -0700

    addded dev mode to webpack config / clean output

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
   },
@@ -13,6 +14,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
