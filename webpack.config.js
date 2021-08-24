e337cbd03169949dc640cea4a71e2d4f49197b7d
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-24 15:23:24 -0700

    changed output file name format to '[name].buindle.js'

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
