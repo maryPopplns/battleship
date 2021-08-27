3219a59481bb02fad15be042c3ecf8c330f9c804
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-27 14:28:45 -0700

    testing scripts

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 -learned the TDD (testing first) approach to programming
 -learned the importance of using pure functions within tests and how that can improve the modularity,easy of refactoring, and reduce bugs within code.
+
+-
--- src/components/game/ship.test.js
+++ src/components/game/ship.test.js
@@ -1,9 +1,7 @@
 import { Ship } from './game.js';
 
-test('contains method named "hit"', () => {
-  expect(Ship()).toHaveProperty('HIT');
+test.skip('invoking hit ', () => {
+  expect().toStrictEqual();
 });
 
-test.skip('"hit" method returns new Array', () => {
-  expect(Ship().hit(1)).toStrictEqual([1]);
-});
+//todo make test for isSunk()
