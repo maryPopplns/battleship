4368d54eb29a128abccfa5b8fc0bbd22fc1d16a9
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-05 10:25:46 -0700

    added error handling for human attacks

--- src/components/game/player.js
+++ src/components/game/player.js
@@ -9,20 +9,29 @@ export default class Player {
   }
   ai_attack(board) {
     const LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    const RANDOM_LETTER = LETTERS[Math.floor(Math.random() * 10)];
-    const RANDOM_NUM = Math.floor(Math.random() * 10);
-    const NEW_COORDINATE = `${RANDOM_LETTER}${RANDOM_NUM}`;
-    if (this.attacks.includes(NEW_COORDINATE)) {
-      this.ai_move(board);
-    } else {
-      return NEW_COORDINATE;
+    try {
+      if (this.player === 'human') {
+        this.attacks = this.#attack_reducer(coordinate);
+        board.receive_attack(coordinate);
+        return coordinate;
+      } else {
+        throw new Error('Player needs to be AI');
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
   human_attack(board, coordinate) {
-    if (this.player === 'human') {
-      this.attacks = this.#attack_reducer(coordinate);
-      board.receive_attack(coordinate);
-      return coordinate;
+    try {
+      if (this.player === 'human') {
+        this.attacks = this.#attack_reducer(coordinate);
+        board.receive_attack(coordinate);
+        return coordinate;
+      } else {
+        throw new Error('Player needs to be a human');
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 }
--- src/components/game/player.test.js
+++ src/components/game/player.test.js
@@ -22,13 +22,13 @@ describe('player functionality', () => {
     expect(FIRST_ATTACK).toMatch('a1');
     expect(SECOND_ATTACK).toMatch('a2');
   });
-  // test('"attack" method enemy marks board correctly for ai player', () => {
-  //   const PLAYER = new Player('ai');
-  //   const BOARD = new Gameboard();
-  //   const FIRST_ATTACK = PLAYER.attack(BOARD);
-  //   const SECOND_ATTACK = PLAYER.attack(BOARD);
-  //   expect(MOCK_RECIEVE_ATTACKS).toHaveBeenCalledTimes(2);
-  //   expect(FIRST_ATTACK).toMatch(/[a-j][0-9]/);
-  //   expect(SECOND_ATTACK).toMatch(/[a-j][0-9]/);
-  // });
+  test('"attack" method enemy marks board correctly for ai player', () => {
+    const PLAYER = new Player('ai');
+    const BOARD = new Gameboard();
+    const FIRST_ATTACK = PLAYER.attack(BOARD);
+    const SECOND_ATTACK = PLAYER.attack(BOARD);
+    expect(MOCK_RECIEVE_ATTACKS).toHaveBeenCalledTimes(2);
+    expect(FIRST_ATTACK).toMatch(/[a-j][0-9]/);
+    expect(SECOND_ATTACK).toMatch(/[a-j][0-9]/);
+  });
 });
