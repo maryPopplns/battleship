a75607431c82d2a2b524483ea3d6b6b1f525bfde
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-04 19:05:39 -0700

    created test for ai attack

--- src/components/game/player.test.js
+++ src/components/game/player.test.js
@@ -20,14 +20,16 @@ describe('player functionality', () => {
     const FIRST_ATTACK = PLAYER.attack(BOARD, 'a1');
     const SECOND_ATTACK = PLAYER.attack(BOARD, 'a2');
     expect(MOCK_RECIEVE_ATTACKS).toHaveBeenCalledTimes(2);
-    expect(FIRST_ATTACK).toStrictEqual('a1');
-    expect(SECOND_ATTACK).toStrictEqual('a2');
+    expect(FIRST_ATTACK).toMatch('a1');
+    expect(SECOND_ATTACK).toMatch('a2');
   });
   test('"attack" method enemy marks board correctly for ai player', () => {
     const PLAYER = new Player('ai');
     const BOARD = new Gameboard();
-    PLAYER.attack(BOARD);
-    PLAYER.attack(BOARD);
+    const FIRST_ATTACK = PLAYER.attack(BOARD);
+    const SECOND_ATTACK = PLAYER.attack(BOARD);
     expect(MOCK_RECIEVE_ATTACKS).toHaveBeenCalledTimes(2);
+    expect(FIRST_ATTACK).toMatch(/[a-j][0-9]/);
+    expect(SECOND_ATTACK).toMatch(/[a-j][0-9]/);
   });
 });
