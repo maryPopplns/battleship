da74f5edb523e7a8880594e1b8ebcdb6de09df33
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-04 17:59:41 -0700

    added test for attacks from player input

--- src/components/game/player.test.js
+++ src/components/game/player.test.js
@@ -8,3 +8,15 @@ jest.mock('./gameboard', () => {
     return { receive_attack: MOCK_RECIEVE_ATTACKS };
   });
 });
+
+describe('player functionality', () => {
+  test('"attack" method enemy marks board correctly for human player', () => {
+    const PLAYER = new Player('human');
+    const BOARD = new Gameboard();
+    PLAYER.attack(BOARD, 'a1');
+    expect(MOCK_RECIEVE_ATTACKS).toHaveBeenCalledWith('a1');
+    PLAYER.attack(BOARD, 'a2');
+    expect(MOCK_RECIEVE_ATTACKS).toHaveBeenCalledWith('a2');
+    expect(MOCK_RECIEVE_ATTACKS).toHaveBeenCalledTimes(2);
+  });
+});
