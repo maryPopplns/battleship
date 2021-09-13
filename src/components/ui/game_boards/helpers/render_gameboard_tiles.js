c10a2f1c176225d175414b64ee5c7de3a8ce19e4
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-13 12:39:08 -0700

    fixed issue with player board not showing whole board when window small

--- src/components/ui/game_boards/helpers/render_gameboard_tiles.js
+++ src/components/ui/game_boards/helpers/render_gameboard_tiles.js
@@ -2,13 +2,11 @@ import COORDINATES from '../../../helpers/coordinates.js';
 
 export default function render_gameboard_tiles() {
   const MAIN = document.getElementById('game_boards');
-  const BOARD_CONTAINER = document.createElement('div');
   const PLAYER_BOARD = document.createElement('div');
   const AI_BOARD = document.createElement('div');
 
   PLAYER_BOARD.classList.add('game_board');
   AI_BOARD.classList.add('game_board');
-  BOARD_CONTAINER.id = 'board_container';
   PLAYER_BOARD.id = 'player_board';
   AI_BOARD.id = 'ai_board';
   for (let i = 0; i < 100; i++) {
@@ -24,7 +22,6 @@ export default function render_gameboard_tiles() {
     AI_BOARD.append(AI_BOARD_TILE);
   }
 
-  BOARD_CONTAINER.append(PLAYER_BOARD);
-  BOARD_CONTAINER.append(AI_BOARD);
-  MAIN.append(BOARD_CONTAINER);
+  MAIN.append(PLAYER_BOARD);
+  MAIN.append(AI_BOARD);
 }
